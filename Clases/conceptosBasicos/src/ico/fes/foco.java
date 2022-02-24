a1d0c32ad9441bcc4fd1eb4965a414cd19cdd740
yahirruiz18 <yahirruiz46@aragon.unam.mx>
2022-02-24 12:40:30 -0600

    Iniciamos el archivo foco.java

---
+++ Clases/conceptosBasicos/src/ico/fes/foco.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author yahir
+ */
+public class foco {
+    private String color;
+    private String tipo;
+    private int tamano;
+
+    public foco() {
+    }
+
+    public foco(String color, String tipo, int tamano) {
+        this.color = color;
+        this.tipo = tipo;
+        this.tamano = tamano;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
